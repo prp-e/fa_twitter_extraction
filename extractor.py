d66a6082162c303a930246f9e119a7c05cc3adcc
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-08 16:03:49 +0330

    Polished the code a little bit

--- extractor.py
+++ extractor.py
@@ -1,13 +1,17 @@
+import argparse
 import twint
 
 class Extractor:
+    """This class is the main extractor"""
     def __init__(keyword):
         self.keyword = keyword 
     
     def extract(self):
+        """This module helps us extract what we need."""
         config = twint.Config() 
         config.Search = self.keyword 
         config.Store_json = True 
+        config.Limit = 1000 
         config.run.Search() 
 
 if __name__ == '__main__':
